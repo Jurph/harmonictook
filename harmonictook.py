b639e6435386d3443990cc702ac3bffe885c9b89
DW <darkwords@gmail.com>
2020-07-12 13:40:53 -0400

    sketching in code basics

--- harmonictook.py
+++ harmonictook.py
@@ -12,6 +12,16 @@ class Player(object):
         self.deck = []
         self.deck.append(Green("wheat field"), Green("bakery"))
 
+    def buy(self, Card):
+        self.bank -= Card.cost
+        self.deck.append(Card)
+
+    def swap(self, Card, otherPlayer, otherCard):
+        self.deck.remove(Card)
+        self.deck.append(otherCard)
+        otherPlayer.deck.append(Card)
+        otherPlayer.deck.remove(otherCard)
+
 # Cards must have a name, cost, a target, a payout amount, and one or more die rolls on which they "hit"
 # Wheat Field costs 1, targets the die-roller, pays out 1, and hits on a 1.
 # Bakery costs 1, targets the bank, pays out 1 to the die-roller who owns it, and hits on a 2 or 3.
@@ -26,18 +36,42 @@ class Card(object):
         self.hits = [0]
 
     def trigger(self, owner):
-        owner.bank += payout
+        owner.bank += self.payout
 
     def __str__(self):
         return("[{}] {}".format(self.hits, self.name))
 
 class Green(Card):
-    def __init__(self, name)
+    def __init__(self, name):
+        self.name = name
 
 
+    def trigger(self, owner):
+        owner.bank += self.payout
+
+class Red(Card):
+    def __init__(self, name):
+        self.name = name
+
+    def trigger(self, owner, dieroller):
+        if dieroller.bank >= self.payout:
+            dieroller.bank -= self.payout
+            owner.bank += self.payout
+        elif dieroller.bank < self.payout:
+            owner.bank += dieroller.bank
+            dieroller.bank = 0
+
+class Blue(Card):
+    def __init__(self, name):
+        self.name = name 
+
+    def trigger(self, owner):
+        owner.bank += self.payout
 
 
 def main():
+    playerlist = []
+    playerlist.append(Player("jurph", 1))
     print("It works.")
 
 if __name__ == "__main__":
