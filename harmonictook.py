9bd17a0b42eb8a53b9c54a7912e5eea1e8387b27
DW <darkwords@gmail.com>
2020-07-11 23:28:43 -0400

    project skeleton

---
+++ harmonictook.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# harmonictook.py - Main game file
+
+def main():
+    print("It works.")
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# harmonictook.py - utility classes
+
+def main():
+    print("It works.")
+
+if __name__ == "__main__":
+    main()
