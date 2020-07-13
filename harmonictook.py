ffd107efb59935c3e1f300d30104ddbe8ce87463
DW <darkwords@gmail.com>
2020-07-12 23:58:40 -0400

    Basic functional test is working.

--- harmonictook.py
+++ harmonictook.py
@@ -10,70 +10,52 @@ class Player(object):
         self.order = 0
         self.bank = 3  # start with 3 coins
         self.deck = []
-        self.deck.append(WheatField())
-        self.deck.append(Bakery())
+        self.deck.append(Green("Wheat Field",1,1,[1]))
+        self.deck.append(Green("Market",1,1,[2,3]))
+        for card in self.deck:
+            card.owner = self
 
     def buy(self, Card):
         self.bank -= Card.cost
+        Card.owner = self
         self.deck.append(Card)
-
+        
     def swap(self, Card, otherPlayer, otherCard):
-        self.deck.remove(Card)
-        self.deck.append(otherCard)
-        otherPlayer.deck.append(Card)
+        Card.owner = otherPlayer
+        otherCard.owner = self
         otherPlayer.deck.remove(otherCard)
+        self.deck.append(otherCard)
+        self.deck.remove(Card)
+        otherPlayer.deck.append(Card)
 
-# Cards must have a name, cost, a target, a payout amount, and one or more die rolls on which they "hit"
+# Cards must have a name, cost, a payer, a payout amount, and one or more die rolls on which they "hit"
 class Card(object):
     def __init__(self):
         self.name = None        # Name should be a string like "Wheat Field"
-        self.target = None      # Target can be 0 (bank), 1 (die roller), or 2 (each other player)
+        self.payer = None       # Payer can be 0 (bank), 1 (die roller), or 2 (each other player)
         self.recipient = None   # Recipient can be 1 (die roller), 2 (each other player), or 3 (owner)
         self.cost = 0           # Cost should be a non-zero integer 
         self.payout = 0         # Payout can be any integer
-        self.hits = [0]         # "Hits" can be one or more integers achievable on 2d6 
+        self.hitsOn = [0]         # "Hits" can be one or more integers achievable on 2d6 
         self.owner = None       # Cards start with no owner 
 
-    def trigger(self, owner):   # When triggered a card should interact with a player's bank
-        owner.bank += self.payout
-                                # By default the card increments the owner's bank by the payout
+    # TODO: card.helptext goes here 
 
     def __str__(self):          # The string method, by default 
-        return("[{}] {}".format(self.hits, self.name))
+        return("[{}] {}".format(str(self.hitsOn), str(self.name)))
 
 class Green(Card):
-    def __init__(self, name):
+    def __init__(self, name=str, cost=int, payout=int, hitsOn=list):
+        Card.__init__(self)
+        self.payer = 0         # Green cards always pay out from the bank (0)
+        self.recipient = 1     # Green cards always pay to the die roller (1)
         self.name = name
-        self.target = 0         # Target can be 0 (bank), 1 (die roller), or 2 (each other player)
-        self.recipient = 1      # Recipient can be 1 (die roller), 2 (each other player), or 3 (owner)
-
-    def __str__(self):
-        return super().__str__()
+        self.cost = cost
+        self.payout = payout
+        self.hitsOn = hitsOn
 
     def trigger(self, owner):   # Green cards increment the owner's bank by the payout
         owner.bank += self.payout
-
-# Maybe we just create a Green("wheat field", 1, 1, 1) ???
-# Its string and trigger() methods are identical to Green()...
-class WheatField(Green):
-    def __init__(self):
-        self.name = "Wheat Field"
-        self.cost = 1           # Cost should be a non-zero integer 
-        self.payout = 1         # Payout can be any integer
-        self.hits = [1]         # "Hits" can be one or more integers achievable on 2d6 
-
-    def __str__(self):
-        return super().__str__()
-
-class Bakery(Green):
-    def __init__(self):
-        self.name = "Bakery"
-        self.cost = 1
-        self.payout = 1
-        self.hits = [2, 3]
-    
-    def __str__(self):
-        return super().__str__()
 
 class Red(Card):
     def __init__(self, name):
@@ -100,12 +82,18 @@ class Store(object):
 
 
 def main():
+    # entities = ["the bank", "the player who rolled the dice", "the other players", "the card owner"]
     playerlist = []
-    playerlist.append(Player("jurph", 1))
+    playerlist.append(Player("Jurph", 1))
     somecards = playerlist[0].deck
     for card in somecards:
-        print("{} has a {}".format(playerlist[0].name, card))
-    print("It works.")
+        print("{} has a {}".format(playerlist[0].name, card.name))
+    thiscard = somecards[0]
+    print("This is a {}. It costs {} and pays out {}.".format(thiscard.name, thiscard.cost, thiscard.payout))
+    print("Right now {} has {} coins.".format(playerlist[0].name, playerlist[0].bank))
+    print("I just rolled a 1!")
+    thiscard.trigger(thiscard.owner)
+    print("Right now {} has {} coins.".format(playerlist[0].name, playerlist[0].bank))
 
 if __name__ == "__main__":
     main()
